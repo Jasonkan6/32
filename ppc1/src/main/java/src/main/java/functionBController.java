50c1c9155729815d2a9aa1276a368cd18cbabcf5
Angel Chiu <lychiuab@connect.ust.hk>
2023-04-18 00:47:40 +0800

    function B UI added

---
+++ ppc1/src/main/java/src/main/java/functionBController.java
@@ -0,0 +1,139 @@
+package src.main.java;
+
+ 
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.text.Text;
+
+import javafx.beans.value.ChangeListener;
+
+ 
+
+public class functionBController {
+
+ 
+
+    @FXML
+
+    private TextField Cap_Grape_tf;
+
+ 
+
+    @FXML
+
+    private TextField Cap_Labor_tf;
+
+ 
+
+    @FXML
+
+    private Text Opt_Grape_Surplus_txt;
+
+ 
+
+    @FXML
+
+    private Text Opt_Labor_Surplus_txt;
+
+ 
+
+    @FXML
+
+    private Text Opt_Noir_txt;
+
+ 
+
+    @FXML
+
+    private Text Opt_Rose_txt;
+
+ 
+
+    @FXML
+
+    private Text Opt_Total_Revenue_txt;
+
+ 
+
+    @FXML
+
+    private Text Opt_Total_txt;
+
+ 
+
+    @FXML
+
+    private TextField Prc_Noir_tf;
+
+ 
+
+    @FXML
+
+    private TextField Prx_Rose_tf;
+
+ 
+
+    @FXML
+
+    private Button exit_btn;
+
+ 
+
+    @FXML
+
+    private Text field_Valid_txt;
+
+ 
+
+    @FXML
+
+    private Button run_btn;
+
+ 
+
+    @FXML
+
+    private Text sys_msg_txt;
+
+ 
+
+    @FXML
+
+    private Text title;
+
+ 
+
+    @FXML
+
+    private TextField weekOfYear_tf;
+
+ 
+
+    @FXML
+
+    void exit_buttonPressed(ActionEvent event) {
+
+ 
+
+    }
+
+ 
+
+    @FXML
+
+    void run_buttonPressed(ActionEvent event) {
+
+ 
+
+    }
+
+ 
+
+}
