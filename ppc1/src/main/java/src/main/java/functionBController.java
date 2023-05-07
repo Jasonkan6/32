b613a75795ef82319d8fe61dc387792f5e001a98
LokYingCHIU <72505096+LokYingCHIU@users.noreply.github.com>
2023-05-07 13:09:07 +0800

    SysMessengerTest and compInitTest update

--- ppc1/src/main/java/src/main/java/functionBController.java
+++ ppc1/src/main/java/src/main/java/functionBController.java
@@ -199,62 +199,52 @@ public class functionBController {
 
     @FXML
     void run_buttonPressed(ActionEvent event) {
-        if (weekOfYear_tf.getText()==""|| Cap_Labor_tf.getText()=="" || Cap_Grape_tf.getText()=="" || Prc_Rose_tf.getText()=="" || Prc_Noir_tf.getText()=="")
+        getDefaultValue();
+
+        Vector<Integer> Opt_Sol = getOpt(Cap_Grape, Grape_Rose, Grape_Noir, Labor_Rose, Labor_Noir, Cap_Labor, Prc_Rose, Prc_Noir);
+        Integer Opt_Rose = Opt_Sol.get(0);
+        Integer Opt_Noir = Opt_Sol.get(1);
+        Integer Opt_Total_Revenue = Opt_Sol.get(2);
+
+        int Sur_Labor = Cap_Labor-(Opt_Rose*Labor_Rose+Opt_Noir*Labor_Noir);
+        int  Sur_Grape = Cap_Grape-(Opt_Rose*Grape_Rose+Opt_Noir*Grape_Noir);
+
+        if (Sur_Labor>0 && Sur_Labor<Labor_Rose && Sur_Labor<Labor_Noir)
         {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("error");
-            alert.setHeaderText("input error");
-            alert.setContentText("Please input all the necessary information.");
-            alert.showAndWait();
+            Sur_Labor = 0;
         }
-        else { // All user input is filled
-            getDefaultValue();
-
-            Vector<Integer> Opt_Sol = getOpt(Cap_Grape, Grape_Rose, Grape_Noir, Labor_Rose, Labor_Noir, Cap_Labor, Prc_Rose, Prc_Noir);
-            Integer Opt_Rose = Opt_Sol.get(0);
-            Integer Opt_Noir = Opt_Sol.get(1);
-            Integer Opt_Total_Revenue = Opt_Sol.get(2);
-
-            int Sur_Labor = Cap_Labor-(Opt_Rose*Labor_Rose+Opt_Noir*Labor_Noir);
-            int  Sur_Grape = Cap_Grape-(Opt_Rose*Grape_Rose+Opt_Noir*Grape_Noir);
-
-            if (Sur_Labor>0 && Sur_Labor<Labor_Rose && Sur_Labor<Labor_Noir)
-            {
-                Sur_Labor = 0;
-            }
-            if (Sur_Grape>0 && Sur_Grape<Grape_Rose && Sur_Grape<Grape_Noir)
-            {
-                Sur_Grape = 0;
-            }
-
-            Opt_Rose_txt.setText(Integer.toString(Opt_Rose));
-            Opt_Noir_txt.setText(Integer.toString(Opt_Noir));
-            Opt_Total_txt.setText(Integer.toString(Opt_Rose+Opt_Noir));
-            Opt_Total_Revenue_txt.setText(Integer.toString(Opt_Total_Revenue));
-            Opt_Labor_Surplus_txt.setText(Integer.toString(Sur_Labor));
-            Opt_Grape_Surplus_txt.setText(Integer.toString(Sur_Grape));
-
-            // System message
-            SysMessenger msg = new SysMessenger();
-            Vector<String> Sys_Msgs = new Vector<>();
-            if (Prod_Cap < Opt_Rose+Opt_Noir)
-            {
-                Sys_Msgs.add("w1: Insufficient production capacity to produce the optimal mix, please reduce or adjust the capacity of labor & grape volume!");
-            }
-            if (Opt_Rose*Grape_Rose + Opt_Noir*Grape_Noir < 0.9*Cap_Grape)
-            {
-                Sys_Msgs.add("w2: Insufficient labor supplied to utilize the grape resource (less than 90%)!");
-            }
-            if (Sur_Labor<0)
-            {
-                Sys_Msgs.add("Er1a: Program bug is occurred, Labor Consumption cannot greater than its capacity!");
-            }
-            if (Sur_Grape<0)
-            {
-                Sys_Msgs.add("Er1b: Program bug is occurred, Grape Consumption cannot greater than its capacity!");
-            }
-            msg.showSystemMessage(sys_msg_txt, Sys_Msgs);
+        if (Sur_Grape>0 && Sur_Grape<Grape_Rose && Sur_Grape<Grape_Noir)
+        {
+            Sur_Grape = 0;
         }
+
+        Opt_Rose_txt.setText(Integer.toString(Opt_Rose));
+        Opt_Noir_txt.setText(Integer.toString(Opt_Noir));
+        Opt_Total_txt.setText(Integer.toString(Opt_Rose+Opt_Noir));
+        Opt_Total_Revenue_txt.setText(Integer.toString(Opt_Total_Revenue));
+        Opt_Labor_Surplus_txt.setText(Integer.toString(Sur_Labor));
+        Opt_Grape_Surplus_txt.setText(Integer.toString(Sur_Grape));
+
+        // System message
+        SysMessenger msg = new SysMessenger();
+        Vector<String> Sys_Msgs = new Vector<>();
+        if (Prod_Cap < Opt_Rose+Opt_Noir)
+        {
+            Sys_Msgs.add("w1: Insufficient production capacity to produce the optimal mix, please reduce or adjust the capacity of labor & grape volume!");
+        }
+        if (Opt_Rose*Grape_Rose + Opt_Noir*Grape_Noir < 0.9*Cap_Grape)
+        {
+            Sys_Msgs.add("w2: Insufficient labor supplied to utilize the grape resource (less than 90%)!");
+        }
+        if (Sur_Labor<0)
+        {
+            Sys_Msgs.add("Er1a: Program bug is occurred, Labor Consumption cannot greater than its capacity!");
+        }
+        if (Sur_Grape<0)
+        {
+            Sys_Msgs.add("Er1b: Program bug is occurred, Grape Consumption cannot greater than its capacity!");
+        }
+        msg.showSystemMessage(sys_msg_txt, Sys_Msgs);
     }
 
 
---
+++ ppc1/src/test/java/src/main/java/SysMessengerTest.java
@@ -0,0 +1,36 @@
+package src.main.java;
+
+import javafx.scene.text.Text;
+import org.junit.Test;
+
+import java.util.Vector;
+
+import static org.junit.Assert.*;
+
+public class SysMessengerTest {
+
+    @Test
+    public void testShowSystemMessage_empty()
+    {
+        SysMessenger sysMessenger = new SysMessenger();
+        Text sys_msg_txt = new Text();
+        Vector<String> messages = new Vector<String>();
+        String messageText = sysMessenger.showSystemMessage(sys_msg_txt, messages);
+        assertEquals("", messageText);
+        assertTrue(sys_msg_txt.isVisible());
+    }
+
+    @Test
+    public void testShowSystemMessage_Valid()
+    {
+        SysMessenger sysMessenger = new SysMessenger();
+        Text sys_msg_txt = new Text();
+        Vector<String> messages = new Vector<String>();
+        messages.add("w1: Insufficient production capacity to produce the optimal mix, please reduce or adjust the capacity of labor & grape volume!");
+        messages.add("Er1a: Program bug is occurred, Labor Consumption cannot greater than its capacity!");
+        String messageText = sysMessenger.showSystemMessage(sys_msg_txt, messages);
+        assertEquals("1. w1: Insufficient production capacity to produce the optimal mix, please reduce or adjust the capacity of labor & grape volume!\n2. Er1a: Program bug is occurred, Labor Consumption cannot greater than its capacity!\n", messageText);
+        assertTrue(sys_msg_txt.isVisible());
+    }
+
+}
